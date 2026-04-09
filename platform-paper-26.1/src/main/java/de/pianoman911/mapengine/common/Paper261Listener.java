3ab676785b35812d0758e5f4b27a843bd1a67d17
pianoman911 <kontakt@finndohrmann.de>
2026-04-09 22:03:06 +0200

    Resolved pull request issues

--- platform-paper-26.1/src/main/java/de/pianoman911/mapengine/common/Paper261Listener.java
+++ platform-paper-26.1/src/main/java/de/pianoman911/mapengine/common/Paper261Listener.java
@@ -6,6 +6,7 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToMessageDecoder;
 import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.protocol.game.ClientboundAnimatePacket;
+import net.minecraft.network.protocol.game.ServerboundAttackPacket;
 import net.minecraft.network.protocol.game.ServerboundInteractPacket;
 import net.minecraft.network.protocol.game.ServerboundSwingPacket;
 import net.minecraft.world.phys.Vec3;
@@ -26,7 +27,7 @@ public final class Paper261Listener extends MessageToMessageDecoder<Packet<?>> {
 
     @Override
     public boolean acceptInboundMessage(Object msg) {
-        return msg instanceof ServerboundInteractPacket || msg instanceof ServerboundSwingPacket;
+        return msg instanceof ServerboundInteractPacket || msg instanceof ServerboundSwingPacket || msg instanceof ServerboundAttackPacket;
     }
 
     @Override
@@ -39,16 +40,24 @@ public final class Paper261Listener extends MessageToMessageDecoder<Packet<?>> {
             passthroughMode = this.bridge.handleInteract(this.player, entityId, pos.x, pos.y, pos.z);
         } else if (msg instanceof ServerboundSwingPacket) {
             passthroughMode = this.bridge.handleSwing(this.player);
-            if (passthroughMode == PassthroughMode.ONLY_ANIMATION) {
-                ClientboundAnimatePacket animatePacket = new ClientboundAnimatePacket(((CraftPlayer) this.player).getHandle(),
-                        ClientboundAnimatePacket.SWING_MAIN_HAND);
-                this.player.getTrackedBy().forEach(player -> ((CraftPlayer) player).getHandle().connection.send(animatePacket));
-            }
+            this.handleAnimation(passthroughMode);
+        } else if (msg instanceof ServerboundAttackPacket(int entityId)) {
+            passthroughMode = this.bridge.handleAttack(this.player, entityId);
+            this.handleAnimation(passthroughMode);
         }
         if (passthroughMode != null && passthroughMode != PassthroughMode.ALL) {
             return;
         }
 
         out.add(msg);
+    }
+
+    private void handleAnimation(PassthroughMode passthroughMode) {
+        if (passthroughMode != PassthroughMode.ONLY_ANIMATION) {
+            return;
+        }
+        ClientboundAnimatePacket animatePacket = new ClientboundAnimatePacket(((CraftPlayer) this.player).getHandle(),
+                ClientboundAnimatePacket.SWING_MAIN_HAND);
+        this.player.getTrackedBy().forEach(player -> ((CraftPlayer) player).getHandle().connection.send(animatePacket));
     }
 }
